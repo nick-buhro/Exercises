088c35ed645845c009d9301dcc5178b0706637a5
Nicholas Buhro <n.buhro@gmail.com>
2016-09-25 20:17:34 +0300

    HackerRank "A or B" problem is solved. (#2)
    * Euler Problem 12 is solved (very slow).
    * Recursive implementation with some errors;(
    * Solution without recursion is implemented. Exists some bugs...
    * AorB is completed.

---
+++ NickBuhro.Exercises.HackerRank/Algorithms/BitManipulation/AorB.cs
@@ -0,0 +1,190 @@
+﻿using System;
+
+namespace HackerRank.Algorithms.BitManipulation
+{
+    /// <summary> 
+    /// <seealso href="https://www.hackerrank.com/challenges/aorb"/>
+    /// </summary>
+    public sealed class AorB
+    {
+        /// <summary>
+        /// Array index represents number from 0 to 15.
+        /// Array value represents how many bits in the number are set to 1.
+        /// </summary>
+        private static readonly int[] BitCountMatrix = {
+            0, // 0000
+            1, // 0001
+            1, // 0010
+            2, // 0011
+            1, // 0100
+            2, // 0101
+            2, // 0110
+            3, // 0111
+            1, // 1000
+            2, // 1001
+            2, // 1010
+            3, // 1011
+            2, // 1100
+            3, // 1101
+            3, // 1110
+            4  // 1111
+        };
+
+        public static void Main()
+        {
+            var q = int.Parse(Console.ReadLine());
+            for (var i = 0; i < q; i++)
+            {
+                var k = int.Parse(Console.ReadLine());
+                var a = Console.ReadLine();
+                var b = Console.ReadLine();
+                var c = Console.ReadLine();
+
+                string aResult;
+                string bResult;
+                
+                if (Calculate(k, a, b, c, out aResult, out bResult))
+                {
+                    Console.WriteLine(aResult);
+                    Console.WriteLine(bResult);
+                }
+                else
+                {
+                    Console.WriteLine("-1");
+                }
+            }
+        }
+
+        internal static bool Calculate(int k, string srcA, string srcB, string srcC, out string tgtA, out string tgtB)
+        {
+            // Prepare vars
+
+            var arrA = srcA.ToCharArray();
+            var arrB = srcB.ToCharArray();
+
+            // First round - mandatory manipulations
+
+            for (var i = 0; i < arrA.Length; i++)
+            {
+                var a = CharToInt(arrA[i]);
+                var b = CharToInt(arrB[i]);
+                var c = CharToInt(srcC[i]);
+
+                // A=0 B=0 C=1 -> B:=1
+                var m = ~a & ~b & c;
+                if (m > 0)
+                {
+                    k -= BitCountMatrix[m];
+                    b = b | m;
+                }
+
+                // A=0 B=1 C=0 -> B:=0
+                m = ~a & b & ~c;
+                if (m > 0)
+                {
+                    k -= BitCountMatrix[m];
+                    b = b & ~m;
+                }
+
+                // A=1 B=0 C=0 -> A:=0
+                m = a & ~b & ~c;
+                if (m > 0)
+                {
+                    k -= BitCountMatrix[m];
+                    a = a & ~m;
+                }
+
+                // A=1 B=1 C=0 -> A:=0 B:=0
+                m = a & b & ~c;
+                if (m > 0)
+                {
+                    k -= (BitCountMatrix[m] << 1);
+                    a = a & ~m;
+                    b = b & ~m;
+                }
+
+                // Finalization
+
+                if (k < 0)
+                {
+                    tgtA = null;
+                    tgtB = null;
+                    return false;
+                }
+
+                arrA[i] = IntToChar(a);
+                arrB[i] = IntToChar(b);
+            }
+
+            // Second round - optional manipulations
+
+            for (var i = 0; i < arrA.Length; i++)
+            {
+                var a = CharToInt(arrA[i]);
+                var b = CharToInt(arrB[i]);
+                var c = CharToInt(srcC[i]);
+
+                var ac = a & c;
+                if (ac == 0) continue;
+
+                for (var m = 8; m > 0; m = m >> 1)
+                {
+                    // A=1 B=1 C=1 -> A:=0
+                    if ((m & ac & b) != 0)
+                    {
+                        if (k > 0)
+                        {
+                            k--;
+                            a = a & ~m;
+                        }
+                    }
+                    // A=1 B=0 C=1 -> A:=0 B:=1
+                    else if ((m & ac & ~b) != 0)
+                    {
+                        if (k > 1)
+                        {
+                            k -= 2;
+                            a = a & ~m;
+                            b = b | m;
+                        }
+                    }
+                }
+
+                //
+                arrA[i] = IntToChar(a);
+                arrB[i] = IntToChar(b);
+            }
+
+            // Prepare results
+
+            tgtA = CharArrayToString(arrA);
+            tgtB = CharArrayToString(arrB);
+            return true;
+        }
+
+        internal static string CharArrayToString(char[] arr)
+        {
+            var i = 0;
+            var iMax = arr.Length - 1;
+            while ((arr[i] == '0') && (i < iMax))
+            {
+                i++;
+            }
+            return new string(arr, i, arr.Length - i);
+        }
+
+        internal static int CharToInt(char c)
+        {
+            if (char.IsDigit(c))
+                return (c - 48);
+            return (c - 55);
+        }
+
+        internal static char IntToChar(int b)
+        {
+            if (b < 10)
+                return (char) (b + 48);
+            return (char) (b + 55);
+        }
+    }
+}
---
+++ NickBuhro.Exercises.HackerRank/Algorithms/BitManipulation/AorBTests.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace HackerRank.Algorithms.BitManipulation
+{
+    public sealed class AorBTests
+    {
+        private struct TestCasePart
+        {
+            public int K { get; set; }
+            public string A { get; set; }
+            public string B { get; set; }
+            public string C { get; set; }
+
+            public bool ResultSuccess { get; set; }
+            public string ResultA { get; set; }
+            public string ResultB { get; set; }
+        }
+
+        private TestCasePart[] ReadTestCase(string name)
+        {
+            TestCasePart[] result;
+            var type = typeof (AorBTests);
+
+            var resourceName = type.FullName + "_" + name + "_Input.txt";
+            using (var stream = type.Assembly.GetManifestResourceStream(resourceName))
+            using (var sr = new StreamReader(stream))
+            {
+                var q = int.Parse(sr.ReadLine());
+                result = new TestCasePart[q];
+                for (var i = 0; i < q; i++)
+                {
+                    result[i] = new TestCasePart
+                    {
+                        K = int.Parse(sr.ReadLine()),
+                        A = sr.ReadLine(),
+                        B = sr.ReadLine(),
+                        C = sr.ReadLine()
+                    };
+                }
+            }
+
+            resourceName = type.FullName + "_" + name + "_Output.txt";
+            using (var stream = type.Assembly.GetManifestResourceStream(resourceName))
+            using (var sr = new StreamReader(stream))
+            {
+                for (var i = 0; i < result.Length; i++)
+                {
+                    var s = sr.ReadLine();
+                    if (s != "-1")
+                    {
+                        result[i].ResultSuccess = true;
+                        result[i].ResultA = s;
+                        result[i].ResultB = sr.ReadLine();
+                    }
+                }
+            }
+
+            return result;
+        }
+        
+        public void Test(string testName)
+        {
+            var testCaseCollection = ReadTestCase(testName);
+            foreach (var tc in testCaseCollection)
+            {
+                string aResult;
+                string bResult;
+
+                var result = AorB.Calculate(tc.K, tc.A, tc.B, tc.C, out aResult, out bResult);
+
+                Assert.Equal(tc.ResultSuccess, result);
+                if (result)
+                {
+                    Assert.Equal(tc.ResultA, aResult);
+                    Assert.Equal(tc.ResultB, bResult);
+                }
+            }
+        }
+
+        [Fact]
+        public void Test00()
+        {
+            Test("TestCase00");
+        }
+
+        [Fact]
+        public void Test20()
+        {
+            Test("TestCase20");
+        }
+
+
+        [Theory]
+        [InlineData('0', 0)]
+        [InlineData('9', 9)]
+        [InlineData('A', 10)]
+        [InlineData('F', 15)]
+        public void ConvertTest(char c, int b)
+        {
+            Assert.Equal(AorB.CharToInt(c), b);
+            Assert.Equal(AorB.IntToChar(b), c);
+        }
+
+        [Theory]
+        [InlineData("A", "A")]
+        [InlineData("AA", "AA")]
+        [InlineData("0A", "A")]
+        [InlineData("0", "0")]
+        [InlineData("00", "0")]
+        [InlineData("000", "0")]
+        public void CharArrayToStringTest(string source, string target)
+        {
+            var a = source.ToCharArray();
+            Assert.Equal(AorB.CharArrayToString(a), target);
+        }
+    }
+}
