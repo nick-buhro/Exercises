68bbbf40b6b17b38a8602b735287ac46b0150ace
Nicholas Buhro <n.buhro@gmail.com>
2016-10-26 21:47:18 +0300

    Leetcode/problem135 (#3)
    * Correct solution. Doesn't pass time limit.
    
    * Split on parts.
    
    * Fast implementation.

---
+++ NickBuhro.Exercises.LeetCode/Problem135.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// 135. Candy
+    /// 
+    /// There are N children standing in a line. Each child is assigned a rating value.
+    /// 
+    /// You are giving candies to these children subjected to the following requirements:
+    /// - Each child must have at least one candy.
+    /// - Children with a higher rating get more candies than their neighbors.
+    ///
+    /// What is the minimum candies you must give?
+    /// 
+    /// <seealso href="https://leetcode.com/problems/candy/"/>
+    /// </summary>
+    public class Problem135
+    {
+        public int Candy(int[] ratings)
+        {
+            var lastIndex = ratings.Length - 1;
+            var result = 0;
+
+            var down = false;
+            var index = 0;
+            var anchorIndex = 0;
+            var anchorQty = 0;
+
+            for (;;)
+            {
+                if (index == lastIndex)
+                {
+                    var n = index - anchorIndex;
+                    var qty = ((n) * (n + 1)) >> 1;
+                    qty += Math.Max(anchorQty, n + 1);
+                    result += qty;
+                    break;
+                }
+                
+                var cmp = ratings[index].CompareTo(ratings[index + 1]);
+                
+                if (cmp == 0)
+                {
+                    var n = index - anchorIndex;
+                    var qty = ((n) * (n + 1)) >> 1;
+                    qty += Math.Max(anchorQty, n + 1);
+                    result += qty;
+
+                    index++;
+                    anchorIndex = index;
+                    anchorQty = 0;
+                    down = false;
+                    continue;
+                }
+
+                if (down)
+                {
+                    if (cmp == -1)
+                    {
+                        var n = index - anchorIndex;
+                        var qty = ((n) * (n + 1)) >> 1;
+                        qty += Math.Max(anchorQty, n + 1);
+                        qty--;
+                        result += qty;
+                        
+                        anchorQty = 0;
+                        anchorIndex = index;
+                        down = false;
+                    }
+                    index++;
+                }
+                else
+                {
+                    if (cmp == 1)
+                    {
+                        var n = index - anchorIndex;
+                        var qty = ((n) * (n + 1)) >> 1;
+                        result += qty;
+
+                        anchorQty = n + 1;
+                        anchorIndex = index;
+                        down = true;
+                    }
+                    index++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ NickBuhro.Exercises.LeetCode/Problem135Tests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace LeetCode
+{
+    public class Problem135Tests
+    {
+        [Fact]
+        public void MyTest()
+        {
+            // Candies:     {1, 4, 3, 2, 1}
+            int[] ratings = {1, 5, 4, 3, 2};
+            const int expected = 11;
+
+            var p = new Problem135();
+            var actual = p.Candy(ratings);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test01()
+        {
+            // Candies:     { 1, 2, 1 }
+            int[] ratings = { 1, 2, 2 };
+            const int expected = 4;
+
+            var p = new Problem135();
+            var actual = p.Candy(ratings);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TimeLimitTest()
+        {
+            var ratings = GetTimeLimitTestRatings();
+            const int expected = 40764;
+
+            var p = new Problem135();
+            var actual = p.Candy(ratings);
+            
+            Assert.Equal(expected, actual);
+        }
+
+        private static int[] GetTimeLimitTestRatings()
+        {
+            var type = typeof(Problem135Tests);
+
+            var resourceName = type.FullName + "_TimeLimitTest.txt";
+            using (var stream = type.Assembly.GetManifestResourceStream(resourceName))
+            using (var sr = new StreamReader(stream))
+            {
+                return sr
+                    .ReadToEnd()
+                    .Split(',')
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+        }
+    }
+}
